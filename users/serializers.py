7a30b93d1831f112b54dce1b11936405e06c2088
= <sasiocln@gmail.com>
2019-06-21 23:33:04 +0600

    views updated

--- users/serializers.py
+++ users/serializers.py
@@ -3,7 +3,7 @@ from . import models
 from allauth.account import app_settings as allauth_settings
 from allauth.utils import email_address_exists
 from allauth.account.adapter import get_adapter
-from allauth.account.utils import setup_user_email
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
